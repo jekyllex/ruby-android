ce062dc7b8a62181dd47a3a75c0c1c138691f696
Gourav Khunger <gouravkhunger18@gmail.com>
2024-06-30 17:34:08 +0530

    fix: add required gems to blank template by default

--- patches/gems/jekyll-3.9.5/lib/jekyll/commands/new.rb
+++ patches/gems/jekyll-3.9.5/lib/jekyll/commands/new.rb
@@ -102,6 +102,9 @@ module Jekyll
             # Lock `http_parser.rb` gem to `v0.6.x` on JRuby builds since newer versions of the gem
             # do not have a Java counterpart.
             gem "http_parser.rb", "~> 0.6.0", :platforms => [:jruby]
+
+            gem "webrick", "~> 1.8"
+            gem "kramdown-parser-gfm", "~> 1.1"
           RUBY
         end
 
