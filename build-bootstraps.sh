713dacb88643721ada155c65005c97a7c9932baa
Gourav Khunger <gouravkhunger18@gmail.com>
2024-07-01 13:12:43 +0530

    chore: finalize

--- build-bootstraps.sh
+++ build-bootstraps.sh
@@ -395,11 +395,11 @@ main() {
 		EXTRACTED_PACKAGES=()
 
 		# Core JekyllEx utilities.
-		# PACKAGES+=("coreutils")
-		# PACKAGES+=("libxslt")
-		# PACKAGES+=("libxml2")
-		# PACKAGES+=("ruby")
-		PACKAGES+=("tar")
+		PACKAGES+=("coreutils")
+		PACKAGES+=("libxslt")
+		PACKAGES+=("libxml2")
+		PACKAGES+=("ruby")
+		PACKAGES+=("git")
 
 		# Handle additional packages.
 		for add_pkg in "${ADDITIONAL_PACKAGES[@]}"; do
