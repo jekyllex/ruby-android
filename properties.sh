bbcab77cb7724aaeb8eaea25014891c12eb23e21
Gourav Khunger <gouravkhunger18@gmail.com>
2024-07-01 12:22:55 +0530

    chore: update app package name

--- properties.sh
+++ properties.sh
@@ -18,8 +18,8 @@ else
 fi
 
 # Termux packages configuration.
-TERMUX_APP_PACKAGE="sh.gourav.jekyllex"
-TERMUX_REPO_PACKAGE="sh.gourav.jekyllex"
+TERMUX_APP_PACKAGE="xyz.jekyllex"
+TERMUX_REPO_PACKAGE="xyz.jekyllex"
 TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
 TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
