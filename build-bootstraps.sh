06ef941bc93bb0dec2b7610bf1108bf3afc78110
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-10 00:25:05 +0530

    feat: ready

--- build-bootstraps.sh
+++ build-bootstraps.sh
@@ -405,10 +405,10 @@ main() {
 
 		# Core JekyllEx utilities.
 		PACKAGES+=("coreutils")
-		# PACKAGES+=("libxslt")
-		# PACKAGES+=("libxml2")
-		# PACKAGES+=("ruby")
-		# PACKAGES+=("git")
+		PACKAGES+=("libxslt")
+		PACKAGES+=("libxml2")
+		PACKAGES+=("ruby")
+		PACKAGES+=("git")
 
 		# Handle additional packages.
 		for add_pkg in "${ADDITIONAL_PACKAGES[@]}"; do
