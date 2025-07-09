073f916a090bd54908d205298b04b57eb74428e3
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-09 23:36:50 +0530

    chore: use HOME

--- run-docker.sh
+++ run-docker.sh
@@ -5,7 +5,8 @@ set -e -u
 
 TERMUX_SCRIPTDIR=$(cd "$(realpath "$(dirname "$0")")"; cd ..; pwd)
 
-CONTAINER_HOME_DIR="/home/runner"
+export HOME="/home/runner"
+CONTAINER_HOME_DIR="$HOME"
 UNAME=$(uname)
 if [ "$UNAME" = Darwin ]; then
 	# Workaround for mac readlink not supporting -f.
