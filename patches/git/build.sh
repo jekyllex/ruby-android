4210717d1f62b302c5471707f249dad3c5335f14
Gourav Khunger <gouravkhunger18@gmail.com>
2026-08-16 14:14:38 +0530

    ci: slim bootstrap workflow and apply properties patch

--- patches/git/build.sh
+++ patches/git/build.sh
@@ -1,99 +1,58 @@
 TERMUX_PKG_HOMEPAGE=https://git-scm.com/
 TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.46.0"
+TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
+TERMUX_PKG_VERSION="2.55.0"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7f123462a28b7ca3ebe2607485f7168554c2b10dfc155c7ec46300666ac27f95
+TERMUX_PKG_SHA256=457fdb04dc8728e007d4688695e6912e6f680727920f2a40bf11eacc17505357
 TERMUX_PKG_AUTO_UPDATE=true
-# removed libexpat, pcre2
-TERMUX_PKG_DEPENDS="libcurl, libiconv, less, openssl, zlib"
-# TERMUX_PKG_RECOMMENDS="openssh"
-# TERMUX_PKG_SUGGESTS="perl"
+TERMUX_PKG_DEPENDS="libcurl, libexpat, libiconv, less, openssl, pcre2, zlib"
+TERMUX_PKG_RECOMMENDS="openssh"
+TERMUX_PKG_SUGGESTS="perl"
 
-## This requires a working $TERMUX_PREFIX/bin/sh on the host building:
-# removed --with-expat --with-tcltk=$TERMUX_PREFIX/bin/wish
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_fread_reads_directories=yes
 ac_cv_header_libintl_h=no
 ac_cv_iconv_omits_bom=no
 ac_cv_snprintf_returns_bogus=no
 --with-curl
+--with-expat
 --with-shell=$TERMUX_PREFIX/bin/sh
 "
-# expat is only used by git-http-push for remote lock management over DAV, so disable:
-# NO_INSTALL_HARDLINKS to use symlinks instead of hardlinks (which does not work on Android M):
-# added: NO_PERL=1, NO_EXPAT=1, NO_TCLTK=1
-# removed: PERL_PATH=$TERMUX_PREFIX/bin/perl, USE_LIBPCRE2=1
 TERMUX_PKG_EXTRA_MAKE_ARGS="
 NO_NSEC=1
-NO_PERL=1
-NO_EXPAT=1
-NO_TCLTK=1
 NO_GETTEXT=1
+NO_TCLTK=1
 NO_INSTALL_HARDLINKS=1
+INSTALL_SYMLINKS=1
+CSPRNG_METHOD=openssl
+USE_LIBPCRE2=1
+DEFAULT_PAGER=pager
+DEFAULT_EDITOR=editor
 "
+TERMUX_PKG_MAKE_INSTALL_TARGET="install install-man"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-# Things to remove to save space:
-#  bin/git-cvsserver - server emulating CVS
-#  bin/git-shell - restricted login shell for Git-only SSH access
-TERMUX_PKG_RM_AFTER_INSTALL="
-bin/git-cvsserver
-bin/git-shell
-libexec/git-core/git-shell
-libexec/git-core/git-cvsserver
-share/man/man1/git-cvsserver.1
-share/man/man1/git-shell.1
-"
-
 termux_step_pre_configure() {
-	# Certain packages are not safe to build on device because their
-	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
-		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
-
-	# Setup perl so that the build process can execute it:
-	# rm -f $TERMUX_PREFIX/bin/perl
-	# ln -s $(command -v perl) $TERMUX_PREFIX/bin/perl
-
-	# Force fresh perl files (otherwise files from earlier builds
-	# remains without bumped modification times, so are not picked
-	# up by the package):
-	rm -Rf $TERMUX_PREFIX/share/git-perl
-
-	# Fixes build if utfcpp is installed:
+	termux_setup_rust
+	export CARGO_BUILD_TARGET="$CARGO_TARGET_NAME"
 	CPPFLAGS="-I$TERMUX_PKG_SRCDIR $CPPFLAGS"
 }
 
+termux_step_make_install() {
+	make -j "${TERMUX_PKG_MAKE_PROCESSES}" ${TERMUX_PKG_EXTRA_MAKE_ARGS} ${TERMUX_PKG_MAKE_INSTALL_TARGET}
+	make -C contrib/subtree -j "${TERMUX_PKG_MAKE_PROCESSES}" ${TERMUX_PKG_EXTRA_MAKE_ARGS} ${TERMUX_PKG_MAKE_INSTALL_TARGET}
+}
+
 termux_step_post_make_install() {
-	# Installing man requires asciidoc and xmlto, so git uses separate make targets for man pages
-	# make -j $TERMUX_PKG_MAKE_PROCESSES install-man
-
-	# make -j $TERMUX_PKG_MAKE_PROCESSES -C contrib/subtree $TERMUX_PKG_EXTRA_MAKE_ARGS
-	# make -C contrib/subtree $TERMUX_PKG_EXTRA_MAKE_ARGS ${TERMUX_PKG_MAKE_INSTALL_TARGET}
-	# make -j $TERMUX_PKG_MAKE_PROCESSES -C contrib/subtree install-man
-
-	# mkdir -p $TERMUX_PREFIX/etc/bash_completion.d/
-	# cp $TERMUX_PKG_SRCDIR/contrib/completion/git-completion.bash \
-	# 	$TERMUX_PKG_SRCDIR/contrib/completion/git-prompt.sh \
-	# 	$TERMUX_PREFIX/etc/bash_completion.d/
-
-	# Remove the build machine perl setup in termux_step_pre_configure to avoid it being packaged:
-	# rm $TERMUX_PREFIX/bin/perl
-
-	# Remove clutter:
-	rm -Rf $TERMUX_PREFIX/lib/*-linux*/perl
-
-	# Remove duplicated binaries in bin/ with symlink to the one in libexec/git-core:
-	(cd $TERMUX_PREFIX/bin; ln -s -f ../libexec/git-core/git git)
-	(cd $TERMUX_PREFIX/bin; ln -s -f ../libexec/git-core/git-upload-pack git-upload-pack)
-	# (cd $TERMUX_PREFIX/libexec/git-core; ln -s -f git-gui git-citool)
+	mkdir -p "$TERMUX_PREFIX/etc/bash_completion.d/"
+	cp "$TERMUX_PKG_SRCDIR/contrib/completion/git-completion.bash" \
+		"$TERMUX_PKG_SRCDIR/contrib/completion/git-prompt.sh" \
+		"$TERMUX_PREFIX/etc/bash_completion.d/"
 }
 
 termux_step_post_massage() {
-	if [ ! -f libexec/git-core/git-remote-https ]; then
+	if [[ ! -f libexec/git-core/git-remote-https ]]; then
 		termux_error_exit "Git built without https support"
 	fi
 }
--- patches/ncurses/build.sh
+++ patches/ncurses/build.sh
@@ -2,39 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://invisible-island.net/ncurses/
 TERMUX_PKG_DESCRIPTION="Library for text-based user interfaces in a terminal-independent manner"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-# This references a commit in https://github.com/ThomasDickey/ncurses-snapshots, specifically
-# https://github.com/ThomasDickey/ncurses-snapshots/commit/${_SNAPSHOT_COMMIT}
-# Check the commit description to see which version a commit belongs to - correct version
-# is checked in termux_step_pre_configure(), so the build will fail on a mistake.
-# Using this simplifies things (no need to avoid downloading and applying patches manually),
-# and uses github is a high available hosting.
-_SNAPSHOT_COMMIT=8bd5a3d98fc741bdcc9e5fada1a3d5980e1ea22a
-TERMUX_PKG_VERSION=(6.4.20231001
-                    9.31
-                    15
-                    0.31.0
-                    0.11.0)
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=(https://github.com/ThomasDickey/ncurses-snapshots/archive/${_SNAPSHOT_COMMIT}.tar.gz
-                   http://dist.schmorp.de/rxvt-unicode/Attic/rxvt-unicode-${TERMUX_PKG_VERSION[1]}.tar.bz2
-                   https://github.com/thestinger/termite/archive/v${TERMUX_PKG_VERSION[2]}.tar.gz
-                   https://github.com/kovidgoyal/kitty/releases/download/v${TERMUX_PKG_VERSION[3]}/kitty-${TERMUX_PKG_VERSION[3]}.tar.xz
-                   https://github.com/alacritty/alacritty/archive/refs/tags/v${TERMUX_PKG_VERSION[4]}.tar.gz)
-TERMUX_PKG_SHA256=(ca4a28ed4d38a7b79e1cd883e3d2755839072a7e4fe8cf265be1ef4ae79b6bc2
-                   aaa13fcbc149fe0f3f391f933279580f74a96fd312d6ed06b8ff03c2d46672e8
-                   3ae9ebef28aad081c6c11351f086776e2fd9547563b2f900732b41c376bec05a
-                   d122497134abab8e25dfcb6b127af40cfe641980e007f696732f70ed298198f5
-                   0fb3370c662f5b87d1b9a487aef999195212b192e08f6f68a572fed8fd637e07)
+_SNAPSHOT_COMMIT=5f58399b2de47ed14bdfe3a0cb149293b27893d5
+TERMUX_PKG_VERSION="6.6.20260307+really6.5.20250830"
+TERMUX_PKG_SRCURL="https://github.com/ThomasDickey/ncurses-snapshots/archive/${_SNAPSHOT_COMMIT}.tar.gz"
+TERMUX_PKG_SHA256="28cd102efe6a2610e830cc79cf270da6ff0427b2022900a9a36d2761522f9576"
 TERMUX_PKG_AUTO_UPDATE=false
 
-# ncurses-utils: tset/reset/clear are moved to package 'ncurses'.
 TERMUX_PKG_BREAKS="ncurses-dev, ncurses-utils (<< 6.1.20190511-4)"
 TERMUX_PKG_REPLACES="ncurses-dev, ncurses-utils (<< 6.1.20190511-4)"
 
-# --disable-stripping to disable -s argument to install which does not work when cross compiling:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_locale_h=no
 am_cv_langinfo_codeset=no
+--disable-opaque-panel
 --disable-stripping
 --enable-const
 --enable-ext-colors
@@ -61,64 +41,56 @@ share/man/man7
 "
 
 termux_step_pre_configure() {
-	MAIN_VERSION=$(cut -f 2 VERSION)
-	PATCH_VERSION=$(cut -f 3 VERSION)
-	ACTUAL_VERSION=${MAIN_VERSION}.${PATCH_VERSION}
-	EXPECTED_VERSION=${TERMUX_PKG_VERSION[0]}
-	if [ "${ACTUAL_VERSION}" != "${EXPECTED_VERSION}" ]; then
+	MAIN_VERSION="$(cut -f 2 VERSION)"
+	PATCH_VERSION="$(cut -f 3 VERSION)"
+	ACTUAL_VERSION="${MAIN_VERSION}.${PATCH_VERSION}"
+	EXPECTED_VERSION="${TERMUX_PKG_VERSION#*really}"
+	if [[ "${ACTUAL_VERSION}" != "${EXPECTED_VERSION}" ]]; then
 		termux_error_exit "Version mismatch - expected ${EXPECTED_VERSION}, was ${ACTUAL_VERSION}. Check https://github.com/ThomasDickey/ncurses-snapshots/commit/${_SNAPSHOT_COMMIT}"
 	fi
 	export CPPFLAGS+=" -fPIC"
 }
 
 termux_step_post_make_install() {
-	cd $TERMUX_PREFIX/lib
+	cd "$TERMUX_PREFIX/lib" || termux_error_exit "Prefix 'lib' directory does not exist."
 
-	# Ncursesw/Ncurses compatibility symlinks.
+	local version="${TERMUX_PKG_VERSION#*really}"
+
 	for lib in form menu ncurses panel; do
-		ln -sfr lib${lib}w.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so.${TERMUX_PKG_VERSION:0:3}
-		ln -sfr lib${lib}w.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so.${TERMUX_PKG_VERSION:0:1}
-		ln -sfr lib${lib}w.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so
-		ln -sfr lib${lib}w.a lib${lib}.a
-		(cd pkgconfig; ln -sf ${lib}w.pc $lib.pc)
+		ln -sfr "lib${lib}w.so.${version:0:3}" "lib${lib}.so.${version:0:3}"
+		ln -sfr "lib${lib}w.so.${version:0:3}" "lib${lib}.so.${version:0:1}"
+		ln -sfr "lib${lib}w.so.${version:0:3}" "lib${lib}.so"
+		ln -sfr "lib${lib}w.a" "lib${lib}.a"
+		(cd pkgconfig; ln -sf "${lib}w.pc" "$lib.pc") || termux_error_exit "Failed to install comatibility symlink for '${lib}'"
 	done
 
-	# Legacy compatibility symlinks (libcurses, libtermcap, libtic, libtinfo).
 	for lib in curses termcap tic tinfo; do
-		ln -sfr libncursesw.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so.${TERMUX_PKG_VERSION:0:3}
-		ln -sfr libncursesw.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so.${TERMUX_PKG_VERSION:0:1}
-		ln -sfr libncursesw.so.${TERMUX_PKG_VERSION:0:3} lib${lib}.so
-		ln -sfr libncursesw.a lib${lib}.a
-		(cd pkgconfig; ln -sfr ncursesw.pc ${lib}.pc)
+		ln -sfr "libncursesw.so.${version:0:3}" "lib${lib}.so.${version:0:3}"
+		ln -sfr "libncursesw.so.${version:0:3}" "lib${lib}.so.${version:0:1}"
+		ln -sfr "libncursesw.so.${version:0:3}" "lib${lib}.so"
+		ln -sfr libncursesw.a "lib${lib}.a"
+		(cd pkgconfig; ln -sfr ncursesw.pc "${lib}.pc") || termux_error_exit "Failed to install legacy comatibility symlink for '${lib}'"
 	done
 
-	# Some packages want these:
-	cd $TERMUX_PREFIX/include/
+	cd "$TERMUX_PREFIX/include/" || termux_error_exit "Prefix 'include' directory does not exist."
 	rm -Rf ncurses{,w}
 	mkdir ncurses{,w}
 	ln -s ../{curses.h,eti.h,form.h,menu.h,ncurses_dll.h,ncurses.h,panel.h,termcap.h,term_entry.h,term.h,unctrl.h} ncurses
 	ln -s ../{curses.h,eti.h,form.h,menu.h,ncurses_dll.h,ncurses.h,panel.h,termcap.h,term_entry.h,term.h,unctrl.h} ncursesw
 
-	# Strip away 30 years of cruft to decrease size.
-	local TI=$TERMUX_PREFIX/share/terminfo
-	mv $TI $TERMUX_PKG_TMPDIR/full-terminfo
-	mkdir -p $TI/{a,d,e,g,n,k,l,p,r,s,t,v,x}
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/a/ansi $TI/a/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/d/{dtterm,dumb} $TI/d/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/e/eterm-color $TI/e/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/g/gnome{,-256color} $TI/g/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/n/nsterm $TI/n/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/k/kitty{,+common,-direct} $TI/k/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/l/linux $TI/l/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/p/putty{,-256color} $TI/p/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/r/rxvt{,-256color} $TI/r/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/s/{screen{,2,-256color},st{,-256color}} $TI/s/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/t/tmux{,-256color} $TI/t/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/v/{vt52,vt100,vt102} $TI/v/
-	cp $TERMUX_PKG_TMPDIR/full-terminfo/x/xterm{,-color,-new,-16color,-256color,+256color} $TI/x/
-
-	tic -x -o $TI $TERMUX_PKG_SRCDIR/rxvt-unicode-${TERMUX_PKG_VERSION[1]}/doc/etc/rxvt-unicode.terminfo
-	tic -x -o $TI $TERMUX_PKG_SRCDIR/termite-${TERMUX_PKG_VERSION[2]}/termite.terminfo
-	tic -x -o $TI $TERMUX_PKG_SRCDIR/kitty-${TERMUX_PKG_VERSION[3]}/terminfo/kitty.terminfo
-	tic -x -o $TI $TERMUX_PKG_SRCDIR/alacritty-${TERMUX_PKG_VERSION[4]}/extra/alacritty.info
+	local TI="$TERMUX_PREFIX/share/terminfo"
+	mv "$TI" "$TERMUX_PKG_TMPDIR/full-terminfo"
+	mkdir -p "$TI"/{a,d,e,g,n,l,p,r,s,t,v,x}
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/a/ansi "$TI/a/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/d/{dtterm,dumb} "$TI/d/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/e/eterm-color "$TI/e/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/g/gnome{,-256color} "$TI/g/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/n/nsterm "$TI/n/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/l/linux "$TI/l/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/p/putty{,-256color} "$TI/p/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/r/rxvt{,-256color} "$TI/r/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/s/{screen{,2,-256color},st{,-256color}} "$TI/s/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/t/tmux{,-256color} "$TI/t/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/v/vt{52,100,102} "$TI/v/"
+	cp "$TERMUX_PKG_TMPDIR"/full-terminfo/x/xterm{,-color,-new,-16color,-256color,+256color} "$TI/x/"
 }
